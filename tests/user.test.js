d999366277e242a1f70207850ecca7014b108f73
ChiaEnChiang <vivian123.cs02@gmail.com>
2019-04-02 11:52:14 +0200

    add more tests and mocks

--- tests/user.test.js
+++ tests/user.test.js
@@ -30,15 +30,29 @@ test("Should signup a new user", async () => {
   // assert that the database is changed correctly
   const user = await User.findById(response.body.user._id);
   expect(user).not.toBeNull();
+  // assert about the response
+  expect(response.body.user.name).toBe("Vivian");
+  expect(response.body).toMatchObject({
+    user: {
+      name: "Vivian",
+      email: "vivian123.cs02@gmail.com"
+    },
+    token: user.tokens[0].token
+  });
+  // see if the password if in the database
+  expect(response.body.user.password).not.toBe("123123123123");
 });
+
 test("Should login existing user", async () => {
-  await request(app)
+  const response = await request(app)
     .post("/user/login")
     .send({
       email: userOne.email,
       password: userOne.password
     })
     .expect(200);
+  const user = await User.findById(response.body.user._id);
+  expect(response.body.token).toBe(user.tokens[1].token);
 });
 
 test("Should not login nonexistent users", async () => {
@@ -72,6 +86,9 @@ test("Should delete account for user", async () => {
     .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200);
+  // assert user in database to be null
+  const user = await User.findById(userOne._id);
+  expect(user).toBeNull();
 });
 test("Should not delete account for an unauthorized user", async () => {
   await request(app)
@@ -79,3 +96,18 @@ test("Should not delete account for an unauthorized user", async () => {
     .send()
     .expect(401);
 });
+
+test("Should upload avatar image", async () => {
+  await request(app)
+    .post("/users/me/avatar")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .attach("avatar", "tests/fixtures/profile-pic.jpg")
+    .expect(200);
+  const user = await User.findById(userOneId);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+// test("Should update valid user fields", async()=>{
+//     await request(app)
+//     .patch
+// })
